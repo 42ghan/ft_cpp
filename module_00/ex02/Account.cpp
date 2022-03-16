09b968bfe0d3dc40d595940d1acd8dc28caf605f
42ghan <ghan@student.42seoul.kr>
2022-03-16 19:04:14 +0900

    mod00_Ex02 : using static getter function

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -70,9 +70,9 @@ int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 // display information of existing accounts
 void Account::displayAccountsInfos(void) {
   _displayTimestamp();
-  std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount
-            << ";deposits:" << _totalNbDeposits
-            << ";withdrawals:" << _totalNbWithdrawals << '\n';
+  std::cout << " accounts:" << getNbAccounts() << ";total:" << _totalAmount
+            << ";deposits:" << getNbDeposits()
+            << ";withdrawals:" << getNbWithdrawals() << '\n';
 }
 
 // make a deposit
