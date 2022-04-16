57e9167e8738d9d7f0b49fb34f7543614e670460
42ghan <ghan@student.42seoul.kr>
2022-04-16 23:36:50 +0900

    mod07_ex00 : done

---
+++ module_07/ex00/whatever.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file whatever.hpp
+ * @author ghan (ghan@student.42seoul.kr)
+ * @brief Test template functions
+ * @date 2022-04-16
+ */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <class T>
+void swap(T &a, T &b) {
+  T tmp;
+
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+
+template <class T>
+T min(T a, T b) {
+  return a < b ? a : b;
+}
+
+template <class T>
+T max(T a, T b) {
+  return a > b ? a : b;
+}
+
+#endif
